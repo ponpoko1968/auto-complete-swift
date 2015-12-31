cc1559c20fe6cf460f76ebad6cab79c57da4358b
Ryo Fujimoto <ryo.fujimoto@gu3.co.jp>
2015-12-31 17:48:44 +0900

    fixed value what swift-compiler-args and offset

--- auto-complete-swift.el
+++ auto-complete-swift.el
@@ -3,27 +3,26 @@
 ;; Keywords:
 
 
-
 (require 'json)
 
 (eval-when-compile (require 'cl))
 (defvar swift-executable "/usr/local/bin/sourcekitten")
-(defvar swift-compiler-args "-sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk")
+(defvar swift-compiler-args "-sdk /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk")
 
 (defun swift-process-exec (command)
   (with-output-to-string
     (with-current-buffer standard-output
-      (apply 'call-process (car command) nil '(t ".swift-error") nil (cdr command)) )))
+      (apply 'call-process (car command) nil t nil (cdr command)))))
 
 (defun swift-process (buffer point)
   (unless (buffer-file-name buffer)
     (return ""))
   (let* ((filename (buffer-file-name buffer))
-         (p      (1+ (- point (point-at-bol))))
-         (col      (1+ (- point (point-at-bol))))
-         (row      (count-lines point (point-min)))
-         (cmd      (list swift-executable "complete"
-                         "--file" filename "--offset" (format "%s" p) "--compilerargs" "--" swift-compiler-args) ))
+         (p point)
+         (col (1+ (- point (point-at-bol))))
+         (row (count-lines point (point-min)))
+         (cmd (list swift-executable "complete"
+                    "--file" filename "--offset" (format "%s" p) "--compilerargs" "--" swift-compiler-args) ))
     (message (format "complete at %s:%s:%s" filename row col))
     (swift-process-exec cmd)))
 
@@ -38,20 +37,19 @@
 (defun swift-get-process-completion-result (string)
   (let* ((json-array-type 'list)
          (json-key-type 'string)
-         (candidates (json-read-from-string string)) )
+         (candidates (json-read-from-string string)))
     (mapcar (lambda (candidate)
-              
-              (cdr (assoc  "name" candidate))) candidates)))
-
-
+              (let* ((name (cdr (assoc "name" candidate)))
+                     (typeName (cdr (assoc "typeName" candidate))))
+                (popup-make-item name
+                                 :symbol typeName
+                                 :summary typeName))) candidates)))
 
 (defvar ac-source-swift-complete
   '((candidates . (swift-get-completions nil ac-point))
     (prefix "[^a-zA-Z0-9_]\\(\\(?:[a-zA-Z_][a-zA-Z0-9_]*\\)?\\)" nil 1)
     (requires . 0)
-    (symbol . "C")
-    ))
-
+    (symbol . "C")))
 
 (defun ac-complete-swift ()
   (interactive)
